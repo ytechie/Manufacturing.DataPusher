1fd118f373d81b68b412817ba2a8eee398357742
Jason Young <jason@ytechie.com>
2014-09-02 09:49:43 -0500

    Moved the IDataPusher interface in from the cmn proj

---
+++ IDataPusher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Manufacturing.Framework.Datasource;
+using Manufacturing.Framework.Dto;
+
+namespace Manufacturing.Framework.DataPusher
+{
+    public interface IDataPusher
+    {
+        void PushRecords(IEnumerable<DatasourceRecord> records);
+    }
+}
