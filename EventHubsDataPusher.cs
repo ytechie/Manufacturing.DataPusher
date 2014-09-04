323cab9d504a18ba0865214f31e9675edab5c817
Jason Young <jason@ytechie.com>
2014-09-04 16:48:47 -0500

    Switched to multiple event hub clients

--- EventHubsDataPusher.cs
+++ EventHubsDataPusher.cs
@@ -9,7 +9,6 @@ using Manufacturing.Framework.Datasource;
 using Manufacturing.Framework.Dto;
 using Microsoft.ServiceBus;
 using Microsoft.ServiceBus.Messaging;
-using Microsoft.ServiceBus.Messaging.Amqp;
 
 namespace Manufacturing.DataPusher
 {
@@ -17,7 +16,8 @@ namespace Manufacturing.DataPusher
     {
         private readonly IDatasourceRecordSerializer _recordSerializer;
 
-        private static EventHubClient _eventHub;
+        private readonly int _processorCount; //cache this to avoid lookups
+        private readonly List<EventHubClient> _eventHubClients;
 
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -25,37 +25,53 @@ namespace Manufacturing.DataPusher
         {
             _recordSerializer = recordSerializer;
 
-            _eventHub = EventHubClient.CreateFromConnectionString(configuration.EventHubConnectionString, configuration.EventHubPath);
+            _processorCount = Environment.ProcessorCount;
+            _eventHubClients = new List<EventHubClient>(_processorCount);
+
+            var connectionStringBuilder =
+                new ServiceBusConnectionStringBuilder(configuration.EventHubConnectionString)
+                {
+                    TransportType = TransportType.Amqp,
+                };
+
+            for (var i = 0; i < _processorCount; i++)
+            {
+                var newConnnection = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString(), configuration.EventHubPath);
+         
+                _eventHubClients.Add(newConnnection);
+            }
+
+            Log.DebugFormat("Initialized {0} Event Hub clients", _processorCount);
         }
 
         public void PushRecords(IEnumerable<DatasourceRecord> records)
         {
             var tasks = new List<Task>();
-            var count = 0;
             long bytes = 0;
 
-            var recs = records.ToList();
+            var recs = records.ToArray();
 
             var sw = new Stopwatch();
             sw.Start();
-            foreach (var record in recs)
+            for(var i = 0; i < recs.Length; i++)
             {
-                var serialized = _recordSerializer.Serialize(new[] {record});
+                var serialized = _recordSerializer.Serialize(new[] {recs[i]});
                 bytes += serialized.Length;
                 var ed = new EventData(serialized);
 
                 //Later, if we want to batch, we could use "DatasourceRecordBatch"
                 ed.Properties.Add("Type", "DatasourceRecord");
 
-                tasks.Add(_eventHub.SendAsync(ed));
-                count++;
+                var client = _eventHubClients[i%_processorCount];
+
+                tasks.Add(client.SendAsync(ed));
             }
             Task.WaitAll(tasks.ToArray());
             sw.Stop();
 
-            var msgsPerSec = (1000.0/sw.ElapsedMilliseconds)*count;
+            var msgsPerSec = (1000.0/sw.ElapsedMilliseconds)*recs.Length;
             Log.DebugFormat("Pushed {0} records to Event Hubs in {1}ms ({2:0} msgs / sec, {3:0} total KB)",
-                count, sw.ElapsedMilliseconds, msgsPerSec, bytes / 1024.0);
+                recs.Length, sw.ElapsedMilliseconds, msgsPerSec, bytes / 1024.0);
         }
     }
 }
